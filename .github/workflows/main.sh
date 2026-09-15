b6c6ba69a044eae56a41005c3794f69a5ffd3a4d
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-09-15 09:31:40 +0300

    add f_check_require

--- .github/workflows/main.sh
+++ .github/workflows/main.sh
@@ -1,19 +1,35 @@
 #!/usr/bin/env bash
 
+function f_check_require() {
+    declare -r required_tools=(
+        vala
+        make{,nsis}
+        sh{fmt,ellcheck}
+    )
+    for tool in "${required_tools[@]}"; do
+        if ! command -v "$tool" >/dev/null; then
+            printf "Missing dependency: %s\n" "${tool}"
+            return 1
+        fi
+    done
+    return 0
+}
+
 function f_setup
 {
     if [[ -f '/etc/os-release' ]]; then
         source '/etc/os-release'
-        if ! command -v clang-tidy >/dev/null; then
+        if ! command -v f_check_require >/dev/null; then
             case ${ID:?} in
                 msys2) return 0 ;;
                 debian | ubuntu) sudo bash -c '
                     apt-get update
-                    apt-get install -y clang-{tidy,format} shfmt {cpp,shell}check meson auto{conf,make} libtool gettext cmake {y,n}asm gperf python3 perl ninja-build pkgconf g++-mingw-w64-x86-64 libgomp1 gcc nsis bison flex make
+                    apt-get install -y sh{fmt,ellcheck} make{,nsis} vala
                 ' ;;
                 fedora | alma) sudo dnf install -y shfmt {cpp,shell}check meson auto{conf,make} libtool gettext cmake {y,n}asm gperf python{,3-pip} perl ninja-build pkgconf mingw64-{libgomp,gcc,gcc-c++,nsisb} bison flex make gcc-c++ ;;
             esac 1>/dev/null
         fi
+        f_check_require
         shellcheck --external-sources "${0}"
         shfmt -ci -fn -i 4 -d "${0}"
     fi
