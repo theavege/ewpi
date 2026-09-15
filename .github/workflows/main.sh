af72363359c199367c3761f49efeddeb1cd43e3a
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-09-15 09:39:33 +0300

    fmt

--- .github/workflows/main.sh
+++ .github/workflows/main.sh
@@ -19,12 +19,12 @@ function f_setup
 {
     if [[ -f '/etc/os-release' ]]; then
         source '/etc/os-release'
-        if ! command -v vala >/dev/null; then
+        if ! command -v valac >/dev/null; then
             case ${ID:?} in
                 msys2) return 0 ;;
                 debian | ubuntu) sudo bash -c '
                     apt-get update
-                    apt-get install -y sh{fmt,ellcheck} make{,nsis} vala
+                    apt-get install -y sh{fmt,ellcheck} nsis valac
                 ' ;;
                 fedora | alma) sudo dnf install -y shfmt {cpp,shell}check meson auto{conf,make} libtool gettext cmake {y,n}asm gperf python{,3-pip} perl ninja-build pkgconf mingw64-{libgomp,gcc,gcc-c++,nsisb} bison flex make gcc-c++ ;;
             esac 1>/dev/null
