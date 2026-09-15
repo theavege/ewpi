7834f3fa630e8448f6d87427fca24cf2c66e5569
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-09-15 10:17:38 +0300

    add archive

--- .github/workflows/main.sh
+++ .github/workflows/main.sh
@@ -25,7 +25,7 @@ function f_setup
                 msys2) return 0 ;;
                 debian | ubuntu) sudo bash -c '
                     apt-get update
-                    apt-get install -y sh{fmt,ellcheck} nsis valac lib{soup-3.0,git2-glib-1.0}-dev
+                    apt-get install -y sh{fmt,ellcheck} nsis valac lib{archive,soup-3.0,git2-glib-1.0}-dev
                 ' ;;
                 fedora | alma) sudo dnf install -y shfmt {cpp,shell}check meson auto{conf,make} libtool gettext cmake {y,n}asm gperf python{,3-pip} perl ninja-build pkgconf mingw64-{libgomp,gcc,gcc-c++,nsisb} bison flex make gcc-c++ ;;
             esac 1>/dev/null
